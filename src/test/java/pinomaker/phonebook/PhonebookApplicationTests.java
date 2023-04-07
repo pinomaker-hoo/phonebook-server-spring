d29e49639ac81da16263f4e23491d78c133295d2
pinomaker-hoo <inhoo23@naver.com>
2023-04-08 00:03:27 +0900

    Init : Create Project

---
+++ src/test/java/pinomaker/phonebook/PhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package pinomaker.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
