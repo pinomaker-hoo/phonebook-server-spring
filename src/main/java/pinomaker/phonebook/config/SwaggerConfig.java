abbceb73c55a815f2fb57e2d777b1e8c6baf283c
pinomaker-hoo <inhoo23@naver.com>
2023-04-08 00:11:42 +0900

    feat : Set Swagger Conig

---
+++ src/main/java/pinomaker/phonebook/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package pinomaker.phonebook.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("Phonebook")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Phonebook API")
+                        .description("Phonebook API Document")
+                        .version("v0.0.1"));
+    }
+}
